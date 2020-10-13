465ec3c4883dbdadbf0bd5bc304d9faeb890a007
manishfsb <mnshbsnt@gmail.com>
2020-10-13 14:14:53 -0400

    Getting input from user and printing the input in the next line

--- Project1.s
+++ Project1.s
@@ -1,13 +1,22 @@
 .data
 
-msg:	.asciiz "ManishBasnet"
+msg:	.asciiz "Input: "
+reply:	.space 10
 
 .text
+
 main:
 
 	li $v0, 4
-	la $a0, msg
+	la $a0, msg			#Print msg by storing it's address in $a0
 	syscall
+	
+	li $v0, 8
+	la $a1, reply			#Store the input
+	
+	li $v0, 4
+	la $a0, 0($a1)			#then print the input				
+	syscall 
 
 	li $v0, 10
 	syscall
