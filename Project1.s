538377cc80bd3c325a35ffdeff85d7542cb9ccab
manishfsb <mnshbsnt@gmail.com>
2020-10-14 20:40:45 -0400

    Finally printing the ith character of a string

--- Project1.s
+++ Project1.s
@@ -1,22 +1,16 @@
 .data
 
-msg:	.asciiz "Input: "
-reply:	.space 10
+msg:	.asciiz "Manish"
+
 
 .text
-
 main:
 
-	li $v0, 4
-	la $a0, msg			#Print msg by storing it's address in $a0
+	li $v0, 11
+	la $s0, msg
+	lb $a0, 5($s0)			
 	syscall
 	
-	li $v0, 8
-	la $a1, reply			#Store the input
-	
-	li $v0, 4
-	la $a0, 0($a1)			#then print the input				
-	syscall 
 
 	li $v0, 10
 	syscall
