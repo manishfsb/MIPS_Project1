e6962f003edf6c8410379fe41fbd7ad9b10c02f7
manishfsb <mnshbsnt@gmail.com>
2020-10-15 13:15:21 -0400

    Trying to print the character only if it is lowercase

--- Project1.s
+++ Project1.s
@@ -1,6 +1,7 @@
 .data
 
 msg:	.asciiz "Input: "
+string:	.asciiz "Valid"
 reply:	.space 11
 
 
@@ -13,14 +14,23 @@ main:
 	li $v0, 8						
 	la $a0, reply
 	li $a1, 10
-	syscall 
-	
-	li $v0, 11
-	la $s1, reply
-	lb $a0, 0($s1) 
 	syscall
 
-	li $v0, 10
+	
+	blt $a0, 58, invalid
+	bgt $a0,122, invalid 
+
+	blt $a0, 97, more
+
+more:	bgt $a0, 90, invalid
+	bgt $a0, 64, small
+
+small:
+	li $v0, 11
+	la $a1, $a0
+	syscall
+
+invalid:li $v0, 10
 	syscall
 	
 
