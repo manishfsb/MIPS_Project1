1212c546a6c5f1d28d8f669fbd04554945e9d9d5
manishfsb <mnshbsnt@gmail.com>
2020-10-15 15:49:33 -0400

    Categorizing characters to numbers, lowercase, uppercase and invalids

--- Project1.s
+++ Project1.s
@@ -17,20 +17,33 @@ main:
 	
 	la $s1, reply
 	lb $a0, 1($s1)
-
-	li $s2, 58
+	
+	li $s7, 47
+	li $s2, 57
 	li $s3, 122
 	li $s4, 97
 	li $s5, 90
-	li $s6, 64	
+	li $s6, 64
+	li $s0, 96	
 
 	blt $a0, $s2, invalid
 	bgt $a0,$s3, invalid 
 
 	blt $a0, $s4, more
 
-more:	bgt $a0, $s5, invalid
+more:	
+	bgt $a0, $s0, Lower
+	bgt $a0, $s5, invalid
+	bgt $a0, $s5, invalid
 	bgt $a0, $s6, Upper
+	bgt $a0, $s2, invalid
+	bgt $a0, $s7, numeric
+
+numeric:
+
+
+Lower:
+
 
 Upper:
 	li $v0, 11
